3be8dfbe84948e8f6295624a4becc782878519a3
KuramaStone <20337549+KuramaStone@users.noreply.github.com>
2024-12-31 18:30:25 -0500

    Updated reflection path

--- src/main/java/com/github/kuramastone/utils/BrushableBlockEntityUtils.java
+++ src/main/java/com/github/kuramastone/utils/BrushableBlockEntityUtils.java
@@ -7,11 +7,12 @@ import java.lang.reflect.Field;
 
 public class BrushableBlockEntityUtils {
 
-    private static final Field itemField;
-    private static final Field lootTableSeedField;
+    private static Field itemField;
+    private static Field lootTableSeedField;
 
     static {
         try {
+            // try standard yarn
             itemField = BrushableBlockEntity.class.getDeclaredField("item");
             itemField.setAccessible(true);
 
@@ -19,7 +20,17 @@ public class BrushableBlockEntityUtils {
             lootTableSeedField.setAccessible(true);
         }
         catch (NoSuchFieldException e) {
-            throw new RuntimeException(e);
+            try {
+                // try obfuscated mappings
+                itemField = BrushableBlockEntity.class.getDeclaredField("field_42812");
+                itemField.setAccessible(true);
+
+                lootTableSeedField = BrushableBlockEntity.class.getDeclaredField("field_42815");
+                lootTableSeedField.setAccessible(true);
+            }
+            catch (NoSuchFieldException ex) {
+                throw new RuntimeException(ex);
+            }
         }
     }
 
